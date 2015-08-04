6c897ab6c9e1f40e0df166822bb521e8266e8f0c
Konstantin Stadler <konstantin.stadler@ntnu.no>
2015-08-04 11:38:21 +0200

    matching two list of names through regex, unmembership

--- country_converter.py
+++ country_converter.py
@@ -16,6 +16,132 @@ COUNTRY_DATA_FILE = os.path.join(
         )
 
 
+def match(list_A, list_B, not_found = 'not_found', enforce_sublist = False):
+    """ Matches the country names given in two lists into a dictionary.
+
+    This function matches names given in list_A to the one provided in list_B using 
+    regular expressions defined in country_data.txt
+
+    Parameters
+    ----------
+    list_A : list
+        Names of countries to identify
+    list_B : list
+        Master list of names for coutnries
+        
+
+    not_found : str, optional
+        Fill in value for not found entries. If None, keep the input value (default: 'not found')
+
+
+    enforce_sublist : boolean, optional
+        If True, all entries in both list are list. 
+        If False(default), only multiple matches are list, rest are strings
+
+    Returns
+    -------
+    dict:
+        A dictionary with a key for every entry in list_A. The value
+        correspond to the matching entry in list_B if found. If there is 
+        a 1:1 correspondence, the value is a str (if enforce_sublist is False),
+        otherwise multiple entries as list.
+
+    """
+    if type(list_A) is str: list_A = [list_A]
+    if type(list_B) is str: list_B = [list_B]
+
+    cc = CountryConverter()
+
+    name_dict_A = dict()
+    match_dict_A = dict()
+
+    for name_A in list_A:
+
+        name_dict_A[name_A] = []
+        match_dict_A[name_A] = []
+
+        for regex in cc.regexes:
+            if(regex.search(name_A)):
+                match_dict_A[name_A].append(regex)
+
+        if len(match_dict_A[name_A]) == 0:
+            logging.warn('Could not identify {} in list_A'.format(name_A))
+            _not_found_entry = name_A if not not_found else not_found
+            name_dict_A[name_A].append(_not_found_entry)
+            if not enforce_sublist: name_dict_A[name_A] = name_dict_A[name_A][0]
+            continue
+
+        if len(match_dict_A[name_A]) > 1:
+            logging.warn('Multiple matches for name {} in list_A'.format(name_A))
+
+        for match in match_dict_A[name_A]:
+            B_matches = 0
+            for name_B in list_B:
+                if(match.search(name_B)):
+                    B_matches+=1
+                    name_dict_A[name_A].append(name_B)
+
+        if B_matches == 0:
+            logging.warn('Could not find any correspondence for {} in list_B'.format(name_A))
+            _not_found_entry = name_A if not not_found else not_found
+            name_dict_A[name_A].append(_not_found_entry)
+
+        if B_matches > 1:
+            logging.warn('Multiple matches for name {} in list_B'.format(name_A))
+
+        if not enforce_sublist and (len(name_dict_A[name_A]) == 1):
+            name_dict_A[name_A] = name_dict_A[name_A][0]
+
+    return name_dict_A
+
+
+def convert(**parameters):
+    """ Wraper around CountryConverter.convert()
+
+    Uses the same paramter. This function has the same performance as
+    CountryConverter.convert for one call; for multiple calls its better to
+    instantiate a common CountryConverter (this avoid loading the source data
+    file multiple times). 
+
+    Note
+    ----
+    A lot of the functioality can also be done directly in pandas dataframes.
+    For example:
+    cc = CountryConverter()
+    names = ['USA', 'SWZ', 'PRI']
+    cc.data[cc.data['ISO3'].isin(names)][['ISO2', 'continent']]
+
+    Parameters
+    ----------
+    names : str or list like
+        Countries in 'src' classification to convert to 'to' classification
+
+    src : str, optional
+        Source classification
+
+    to : str or list, optional
+        Output classification (valid str or list of string of the country_data.txt), defalut: name_standard
+
+    enforce_list : boolean, optional
+        If True, enforces the output to be list (if only one name was passed) or to 
+        be a list of lists (if multiple names were passed).
+        If False (default), the output will be a string (if only one name was passed) or
+        a list of str and/or lists (str if a one to one matching, list otherwise).
+
+    not_found : str, optional
+        Fill in value for not found entries. If None, keep the input value (default: 'not found')
+
+    Returns
+    -------
+    list or str, depending on enforce_list
+
+
+    """
+
+    cc = CountryConverter()
+    return cc.convert(**parameters)
+
+
 class CountryConverter():
     """ Main class for converting countries
 
@@ -157,6 +283,23 @@ class CountryConverter():
             to = [to]
         return self.data[self.data.OECD > 0][to]
 
+    def UNin(self, to = 'name_standard'):
+        """ 
+        Return UN memberstates in the specified classification
+
+        Parameters
+        ----------
+        to : str or list
+            Valid column header (first row of country_data.txt).
+
+        Returns
+        -------
+        pandas dataframe
+
+        """
+        if type(to) is str:
+            to = [to]
+        return self.data[self.data.UNmember > 0][to]
 
 
     @property
@@ -173,6 +316,11 @@ class CountryConverter():
     def OECD(self):
         """ OECD memberstates (standard name) - use OECDin() for any other classification """
         return self.OECDin(to = 'name_standard')
+
+    @property
+    def UN(self):
+        """ UN memberstates (standard name) - use UNin() for any other classification """
+        return self.UNin(to = 'name_standard')
 
     @property
     def valid_class(self):
--- develop.py
+++ develop.py
@@ -7,18 +7,38 @@ cc = coco.CountryConverter()
 
 #testnames = ['USA', 'VUT', 'TKL', 'AUT' ]
 #names = ['USA', 'VUT', 'TKL', 'AUT' ]
-testnames = ['US', 'TW', 'WA']
-testnames = ['USA', 'VUT', 'TKL', 'AUT', 'Vietnam' ]
-outformat = ['name_standard', 'ISO3']
-outformat = ['name_standard']
+#testnames = ['US', 'TW', 'WA']
+#testnames = ['Vietnam' ]
+
+#Btest = cc.data.name_standard
+#Atest = cc.data.name_official
+#outformat = ['name_standard', 'ISO3']
+
+
+#Atest={
+#'China',
+#'Republic of China',
+#'Peoples Republic of China',
+#}
+#Btest={
+#'China',
+#'Taiwan',
+#}
+
+#outformat = ['name_standard']
 #out = cc.convert(names = testnames, src = 'ISO3', to = outformat, enforce_list = False, not_found = 'asd')
+#out = coco.convert(names = testnames, src = 'ISO2', to = outformat, enforce_list = False, not_found = 'asd')
+#A,B = coco.zip_lists(Atest, Btest)
+#tt = coco.match(Atest, Btest)
 
-dd = cc.data
-dd['test'] = cc.convert(dd.name_official, src='regex')
 
+#TODO: check with albani with same regex as afghan - should fill in both than
 
 #testnames = ['zanzi', 'korea', 'us']
 #testnames = ['austria']
 #testnames = 'a'
 #outformat = ['name_standard']
 #out = cc.convert(names = testnames, src = 'regex', to = outformat, enforce_list = False, not_found = None)
+
+dd = cc.data
+
