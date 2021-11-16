7731a0b7873e59d9f43bd1928f17d4e148e8dc86
Konstantin Stadler <konstantin.stadler@ntnu.no>
2021-11-16 15:22:11 +0100

    finally removed breakpoint

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -176,8 +176,6 @@ def test_special_cases():
     converter = coco.CountryConverter().convert
 
     # issue 22 - namibia iso2 na interpreted as not a number
-    breakpoint()  # DEBUG
-
     assert converter("NA", to="ISO3") == "NAM"
     assert converter("NAM", to="ISO2") == "NA"
 
